e4e4c69d2b5495c88d1b0204e79bedc1f5d46233
Sam Smets <sam.smets@nix-digital.com>
2024-06-26 15:28:48 +0800

    Use correct path provider package

--- packages/audioplayers_darwin/ios/Classes/AudioContext.swift
+++ packages/audioplayers_darwin/ios/Classes/AudioContext.swift
@@ -1,115 +1,139 @@
 import MediaPlayer
 
 struct AudioContext {
-  let category: AVAudioSession.Category
-  let options: [AVAudioSession.CategoryOptions]
+    let category: AVAudioSession.Category
+    let options: [AVAudioSession.CategoryOptions]
 
-  init() {
-    self.category = .playback
-    self.options = []
-  }
-
-  init(
-    category: AVAudioSession.Category,
-    options: [AVAudioSession.CategoryOptions]
-  ) {
-    self.category = category
-    self.options = options
-  }
-
-  func activateAudioSession(
-    active: Bool
-  ) throws {
-    let session = AVAudioSession.sharedInstance()
-    try session.setActive(active)
-  }
-
-  func apply() throws {
-    let session = AVAudioSession.sharedInstance()
-    let combinedOptions = options.reduce(AVAudioSession.CategoryOptions()) {
-      [$0, $1]
-    }
-    try session.setCategory(category, options: combinedOptions)
-  }
-
-  static func parse(args: [String: Any]) throws -> AudioContext? {
-    guard let categoryString = args["category"] as! String? else {
-      throw AudioPlayerError.error("Null value received for category")
-    }
-    guard let category = try parseCategory(category: categoryString) else {
-      return nil
+    init() {
+        self.category = .playback
+        self.options = []
     }
 
-    guard let optionStrings = args["options"] as! [String]? else {
-      throw AudioPlayerError.error("Null value received for options")
-    }
-    let options = try optionStrings.compactMap {
-      try parseCategoryOption(option: $0)
-    }
-    if optionStrings.count != options.count {
-      return nil
+    init(
+        category: AVAudioSession.Category,
+        options: [AVAudioSession.CategoryOptions]
+    ) {
+        self.category = category
+        self.options = options
     }
 
-    return AudioContext(
-      category: category,
-      options: options
-    )
-  }
-
-  private static func parseCategory(category: String) throws -> AVAudioSession.Category? {
-    switch category {
-    case "ambient":
-      return .ambient
-    case "soloAmbient":
-      return .soloAmbient
-    case "playback":
-      return .playback
-    case "record":
-      return .record
-    case "playAndRecord":
-      return .playAndRecord
-    case "multiRoute":
-      return .multiRoute
-    default:
-      throw AudioPlayerError.error("Invalid Category \(category)")
+    func activateAudioSession(
+        active: Bool
+    ) throws {
+        let session = AVAudioSession.sharedInstance()
+        try session.setActive(active)
     }
-  }
 
-  private static func parseCategoryOption(option: String) throws -> AVAudioSession.CategoryOptions?
-  {
-    switch option {
-    case "mixWithOthers":
-      return .mixWithOthers
-    case "duckOthers":
-      return .duckOthers
-    case "allowBluetooth":
-      return .allowBluetooth
-    case "defaultToSpeaker":
-      return .defaultToSpeaker
-    case "interruptSpokenAudioAndMixWithOthers":
-      return .interruptSpokenAudioAndMixWithOthers
-    case "allowBluetoothA2DP":
-      if #available(iOS 10.0, *) {
-        return .allowBluetoothA2DP
-      } else {
-        throw AudioPlayerError.warning(
-          "Category Option allowBluetoothA2DP is only available on iOS 10+")
-      }
-    case "allowAirPlay":
-      if #available(iOS 10.0, *) {
-        return .allowAirPlay
-      } else {
-        throw AudioPlayerError.warning("Category Option allowAirPlay is only available on iOS 10+")
-      }
-    case "overrideMutedMicrophoneInterruption":
-      if #available(iOS 14.5, *) {
-        return .overrideMutedMicrophoneInterruption
-      } else {
-        throw AudioPlayerError.warning(
-          "Category Option overrideMutedMicrophoneInterruption is only available on iOS 14.5+")
-      }
-    default:
-      throw AudioPlayerError.error("Invalid Category Option \(option)")
+    func apply() throws {
+        let session = AVAudioSession.sharedInstance()
+        let combinedOptions = options.reduce(AVAudioSession.CategoryOptions()) {
+            [$0, $1]
+        }
+        try session.setCategory(category, options: combinedOptions)
     }
-  }
+
+    static func parse(args: [String: Any]) throws -> AudioContext? {
+        guard let categoryString = args["category"] as? String else {
+            throw AudioPlayerError.error("Null value received for category")
+        }
+        guard let category = try parseCategory(category: categoryString) else {
+            return nil
+        }
+
+        guard let optionStrings = args["options"] as? [String] else {
+            throw AudioPlayerError.error("Null value received for options")
+        }
+        let options = try optionStrings.compactMap {
+            try parseCategoryOption(option: $0)
+        }
+        if optionStrings.count != options.count {
+            return nil
+        }
+
+        return AudioContext(
+            category: category,
+            options: options
+        )
+    }
+
+    private static func parseCategory(category: String) throws -> AVAudioSession.Category? {
+        switch category {
+        case "ambient":
+            return .ambient
+        case "soloAmbient":
+            return .soloAmbient
+        case "playback":
+            return .playback
+        case "record":
+            return .record
+        case "playAndRecord":
+            return .playAndRecord
+        case "multiRoute":
+            return .multiRoute
+        default:
+            throw AudioPlayerError.error("Invalid Category \(category)")
+        }
+    }
+
+    private static func parseCategoryOption(option: String) throws -> AVAudioSession.CategoryOptions? {
+        switch option {
+        case "mixWithOthers":
+            return .mixWithOthers
+        case "duckOthers":
+            return .duckOthers
+        #if os(tvOS)
+        case "allowBluetooth":
+            if #available(tvOS 17.0, *) {
+                return .allowBluetooth
+            } else {
+                throw AudioPlayerError.warning(
+                    "Category Option allowBluetooth is only available on tvOS 17+")
+            }
+        case "allowBluetoothA2DP":
+            if #available(tvOS 17.0, *) {
+                return .allowBluetoothA2DP
+            } else {
+                throw AudioPlayerError.warning(
+                    "Category Option allowBluetoothA2DP is only available on tvOS 17+")
+            }
+        #else
+        case "allowBluetooth":
+            return .allowBluetooth
+        case "allowBluetoothA2DP":
+            if #available(iOS 10.0, *) {
+                return .allowBluetoothA2DP
+            } else {
+                throw AudioPlayerError.warning(
+                    "Category Option allowBluetoothA2DP is only available on iOS 10+")
+            }
+        case "allowAirPlay":
+            if #available(iOS 10.0, *) {
+                return .allowAirPlay
+            } else {
+                throw AudioPlayerError.warning("Category Option allowAirPlay is only available on iOS 10+")
+            }
+        #endif
+        case "defaultToSpeaker":
+            #if !os(tvOS)
+            return .defaultToSpeaker
+            #else
+            throw AudioPlayerError.warning("Category Option defaultToSpeaker is unavailable on tvOS")
+            #endif
+        case "interruptSpokenAudioAndMixWithOthers":
+            return .interruptSpokenAudioAndMixWithOthers
+        case "overrideMutedMicrophoneInterruption":
+            #if !os(tvOS)
+            if #available(iOS 14.5, *) {
+                return .overrideMutedMicrophoneInterruption
+            } else {
+                throw AudioPlayerError.warning(
+                    "Category Option overrideMutedMicrophoneInterruption is only available on iOS 14.5+")
+            }
+            #else
+            throw AudioPlayerError.warning("Category Option overrideMutedMicrophoneInterruption is unavailable on tvOS")
+            #endif
+        default:
+            throw AudioPlayerError.error("Invalid Category Option \(option)")
+        }
+    }
 }
--- packages/audioplayers_darwin/ios/audioplayers_darwin.podspec
+++ packages/audioplayers_darwin/ios/audioplayers_darwin.podspec
@@ -5,17 +5,27 @@
 Pod::Spec.new do |s|
   s.name             = 'audioplayers_darwin'
   s.version          = '0.0.1'
-  s.summary          = 'iOS implementation of audioplayers, a Flutter plugin to play multiple audio files simultaneously.'
-  s.description      = 'iOS implementation of audioplayers, a Flutter plugin to play multiple audio files simultaneously.'
+  s.summary          = 'iOS and tvOS implementation of audioplayers, a Flutter plugin to play multiple audio files simultaneously.'
+  s.description      = 'iOS and tvOS implementation of audioplayers, a Flutter plugin to play multiple audio files simultaneously.'
   s.homepage         = 'https://github.com/bluefireteam/audioplayers'
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Blue Fire' => 'contact@blue-fire.xyz' }
   s.source           = { :path => '.' }
-  s.source_files = 'Classes/**/*'
-  s.dependency 'Flutter'
-  s.platform = :ios, '9.0'
+  s.source_files     = 'Classes/**/*'
+  s.dependency       'Flutter'
+  s.swift_version    = '5.0'
 
-  # Flutter.framework does not contain a i386 slice.
-  s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
-  s.swift_version = '5.0'
+  s.platform         = :ios, '9.0'
+  s.tvos.platform    = :tvos, '9.0'
+
+  # Flutter.framework does not contain an i386 slice.
+  s.pod_target_xcconfig = { 
+    'DEFINES_MODULE' => 'YES', 
+    'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' 
+  }
+
+  s.tvos.pod_target_xcconfig = {
+    'DEFINES_MODULE' => 'YES',
+    'EXCLUDED_ARCHS[sdk=appletvsimulator*]' => 'i386'
+  }
 end
